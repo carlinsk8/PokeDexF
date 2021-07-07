c7920e7fdf62e9e686faa40fc53ac2074a3ceb8f
creegplay <carlin.08.12@gmail.com>
2021-07-06 20:44:55 -0500

    Initial Commit

---
+++ Shared/PokeDexFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexFApp.swift
+//  Shared
+//
+//  Created by Carlos on 6/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
